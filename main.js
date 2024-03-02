569f85da4480672758b7644b5b270c64fa20e542
juan12331 <146020152+juan12331@users.noreply.github.com>
2024-03-02 20:40:26 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,27 +2,21 @@ const input = document.getElementById("input");
 const  ADDbutton = document.getElementById("adicionar");
 // const adicionarTexto = document.getElementById("adicionarTexto") 
 
-const addBotao = () => {
-    console.log(input.value) // <-- pegar o valor escrito no input
-    const text = document.createElement("div")
-    const RemoveButton = document.createElement("button")
-    RemoveButton.textContent = "Remover"
-    text.innerText = input.value
-    text.appendChild(RemoveButton)
-    document.body.appendChild(text)
-    input.value = ''
-}
-
-
-
-const adicionar_elemento = (texto) => {
-    // adicionarTexto.innerText = texto
-}
 
 ADDbutton.addEventListener("click", () => {
     if (input.value == ''){
         window.alert("insira algo no input");
     } else{
-    addBotao()
+        console.log(input.value); // <-- pegar o valor escrito no input
+        var text = document.createElement("div");
+        var RemoveButton = document.createElement("button");
+        RemoveButton.textContent = "Remover";
+        text.innerText = input.value;
+        text.appendChild(RemoveButton);
+        document.body.appendChild(text);
+        input.value = '';
     }
-})
+    RemoveButton.addEventListener('click', () =>{
+        text.remove();
+    })
+});
